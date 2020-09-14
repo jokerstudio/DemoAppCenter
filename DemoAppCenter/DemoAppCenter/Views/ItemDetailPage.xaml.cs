19da325457ce65ab6dda3f1eed7fa4f1a55ae3f5
Kritsada Dechawantana <kritsada@perfenterprise.com>
2020-09-14 09:39:27 +0700

    Initial commit

---
+++ DemoAppCenter/DemoAppCenter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using DemoAppCenter.ViewModels;
+
+namespace DemoAppCenter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
